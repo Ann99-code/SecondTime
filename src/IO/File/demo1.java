259d0ad5d55f6ddffdf25ad820d02dc3985af5e6
tom_glo <goodmorning_glo@qq.com>
2022-07-06 00:43:08 +0800

    1

---
+++ src/IO/File/demo1.java
@@ -0,0 +1,10 @@
+package IO.File;
+
+import java.io.File;
+
+public class demo1 {
+    public static void main(String[] args) {
+        File file = new File("F:\\ceshi\\successful.txt");
+        System.out.println(file);
+    }
+}
